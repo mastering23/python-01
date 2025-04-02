981087a2c9da864d8b00a3978142c21c9a875699
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-02 17:39:44 -0400

    adding numbers operations | math module operations

---
+++ src/ex02.py
@@ -0,0 +1,44 @@
+import math
+#numbers Opertations
+print('\n-------Operations------\n')
+num = 1 # interger
+decimal = 1.2 # float
+complex_number = 3 + 4j # complex number
+
+print(1 + 2)
+print(12 - 2)
+print(1 * 2)
+print(12 / 2)
+print(12 // 2)
+print(12 % 2)
+
+#---------------------------------#
+#           function
+#---------------------------------#
+
+print('\n-------Functions-------\n')
+
+print(round(1.3))# round to near number 1
+print(round(1.7))# round to near base on the range 5 t0 9 which is 2
+print(round(1.5))# round to 2
+
+#Absolulity
+print('\n-------Absolute Value-------\n')
+print(abs(-77))
+print(abs(55))
+
+
+#import math
+print('\n-------Math module-------\n')
+print(math.ceil(1.1))# Highest near number
+print(math.floor(1.999))# lowest near number
+print(math.isnan(2))# is not a number ? -> return true otherwise false
+print(math.pow(2,3))# 2**3
+print(math.sqrt(2))# square root
+
+
+
+
+
+
+
